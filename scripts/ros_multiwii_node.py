cbde5d5dac23c71382c2277a01e68acb1d86fac3
Josh Roy <joshnroy@gmail.com>
2017-04-18 17:29:16 +0000

    Started integrating imu

--- scripts/ros_multiwii_node.py
+++ scripts/ros_multiwii_node.py
@@ -7,23 +7,42 @@ from sensor_msgs.msg import Imu
 from pidrone_pkg.msg import RC
 import sys
 import tf
+import numpy as np
+import time
 
 board = MultiWii("/dev/ttyACM0")
 
 cmds = [1500, 1500, 1500, 1000, 1500, 1500, 1500, 1500]
+int_pose = Pose()
+int_vel = [0, 0, 0]
+int_pos = [0, 0, 0]
+
+millis = lambda: int(round(time.time() * 1000))
+
+# rotate vector v1 by quaternion q1 
+def qv_mult(q1, v1):
+    v1 = tf.transformations.unit_vector(v1)
+    q2 = list(v1)
+    q2.append(0.0)
+    return tf.transformations.quaternion_multiply(
+        tf.transformations.quaternion_multiply(q1, q2), 
+        tf.transformations.quaternion_conjugate(q1)
+    )[:3]
 
 def att_pub():
     global cmds
     imupub = rospy.Publisher('/pidrone/imu', Imu, queue_size=1)
+    intposepub = rospy.Publisher('/pidrone/int_pose', Pose, queue_size=1)
     rate = rospy.Rate(300)
     imu = Imu()
-    board.arm()
+#   board.arm()
     print("armed")
+    prev_time = millis()
     while not rospy.is_shutdown():
         att_data = board.getData(MultiWii.ATTITUDE)
-        print(att_data)
+#       print(att_data)
         imu_data = board.getData(MultiWii.RAW_IMU)
-#       print cmds[0], cmds[1], cmds[2], cmds[3]
+        print cmds[0], cmds[1], cmds[2], cmds[3]
         board.sendCMD(16, MultiWii.SET_RAW_RC, cmds)
 
 
@@ -41,10 +60,26 @@ def att_pub():
         imu.orientation.y = quaternion[1]
         imu.orientation.z = quaternion[2]
         imu.orientation.w = quaternion[3]
-        imu.angular_velocity.x = board.rawIMU['ax']
-        imu.angular_velocity.y = board.rawIMU['ay']
-        imu.angular_velocity.z = board.rawIMU['az']
+        imu.linear_acceleration.x = board.rawIMU['gx']
+        imu.linear_acceleration.y = board.rawIMU['gy']
+        imu.linear_acceleration.z = board.rawIMU['gz']
+
+
+# Integrate the things
+        rotated_accel = qv_mult(quaternion, np.array([imu.linear_acceleration.x,
+        imu.linear_acceleration.y, imu.linear_acceleration.z]))
+
+        curr_time = millis()
+        for i in range(len(int_vel)):
+            int_vel[i] += rotated_accel[i] * (curr_time - prev_time)
+            int_pos[i] += int_vel[i] * (curr_time - prev_time)
+        prev_time = curr_time
+        int_pose.orientation = imu.orientation
+        int_pose.position.x = int_pos[0]
+        int_pose.position.y = int_pos[1]
+        int_pose.position.z = int_pos[2]
         imupub.publish(imu)
+        intposepub.publish(int_pose)
     board.disarm()
     print("disarming")
 
