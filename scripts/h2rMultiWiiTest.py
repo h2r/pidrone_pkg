487b87a0c7fd6b0bf83d9c6a4f45efc7d884f6ae
Stefanie Tellex <stefie10@cs.brown.edu>
2017-06-14 09:13:04 -0400

    forking multiwii stuff

---
+++ scripts/h2rMultiWiiTest.py
@@ -0,0 +1,36 @@
+from pyMultiwii import MultiWii
+import time
+
+print "making board."
+board = MultiWii("/dev/ttyACM0")
+
+print "arming."
+#board.arm()
+
+print "armed."
+
+for i in range(1000):
+    print "Ident: "
+    print board.getData(MultiWii.IDENT)
+    print "Attitude: "
+    print board.getData(MultiWii.ATTITUDE)
+
+    print "IMU: "
+    print board.getData(MultiWii.RAW_IMU)
+    print "Status: "
+    print board.getData(MultiWii.STATUS)
+
+    #print "POS_EST: "
+    #print board.getData(MultiWii.POS_EST)
+
+    time.sleep(0.1)
+
+
+    
+
+print "disarm"
+#board.disarm()
+board.ser.close()
+
+
+
