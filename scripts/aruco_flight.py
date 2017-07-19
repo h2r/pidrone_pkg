3b8421859bec32d5a4c44fc74a2200312046d5ab
Josh Roy <joshnroy@gmail.com>
2017-07-19 16:14:51 +0000

    Integrates properly

--- scripts/aruco_flight.py
+++ scripts/aruco_flight.py
@@ -25,6 +25,7 @@ marker_edge_length = 15.5
 kp1=None
 des1=None
 flann_index_kdtree = 0
+vrpn_pos = None
 
 def detectArucoMarker(img):
     corners, ids, regjected = cv2.aruco.detectMarkers(img, aruco_dict)
@@ -36,6 +37,17 @@ def detectArucoMarker(img):
         rot = cv2.Rodrigues(rvecs)[0]   # get the rotation matrix
         ret = rot, tvecs[0][0]
     return ret
+
+def decompose_pose(p):
+    q = Quaternion(p.pose.orientation.w, p.pose.orientation.x,
+        p.pose.orientation.y, p.pose.orientation.z)
+    T = np.identity(4)
+    T[0, 3] = p.pose.position.x
+    T[1, 3] = p.pose.position.y
+    T[2, 3] = p.pose.position.z
+    R = q.transformation_matrix
+    return np.dot(R, T)
+
 
 def compose_pose(RT):
     pos = PoseStamped()
@@ -72,11 +84,19 @@ def arucoRT(img):
     else:
         return None
 
+
+def vrpn_callback(data):
+    global vrpn_pos
+    if vrpn_pos is None:
+        vrpn_pos = decompose_pose(data)
+    
+
 if __name__ == '__main__':
     rospy.init_node("aruco_flight")
     prepospub = rospy.Publisher('/pidrone/aruco_est_pre', PoseStamped, queue_size=1)
     postpospub = rospy.Publisher('/pidrone/aruco_est_post', PoseStamped, queue_size=1)
     homopospub = rospy.Publisher('/pidrone/homo_trans', PoseStamped, queue_size=1)
+    rospy.Subscriber("/pidrone/est_pos", PoseStamped, vrpn_callback)
     prev_img = None
     prev_RT = None
     key = ''
@@ -87,21 +107,28 @@ if __name__ == '__main__':
             print "first prev"
             prev_img = deepcopy(curr_img)
         else:
-            curr_RT = arucoRT(curr_img)
-            prev_RT = arucoRT(prev_img)
+            curr_RT = vrpn_pos
+            prev_RT = vrpn_pos
+            # curr_RT = arucoRT(curr_img)
+            # prev_RT = arucoRT(prev_img)
             # cv2.imshow("curr", curr_img)
             # cv2.imshow("prev", prev_img)
             # cv2.waitKey(1)
-            if prev_RT is not None and curr_RT is not None:
+            # if prev_RT is not None and curr_RT is not None:
+            if vrpn_pos is not None:
                 prev_pos = compose_pose(prev_RT)
                 curr_pos = compose_pose(curr_RT)
                 homography.updateHNew(curr_img, prev_img=prev_img)
                 key = ''
                 init_R = np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
-                while key != 'n':
-                    for i in range(4):
-                        homo_RTn = homography.get_pose_alt(prev_RT, i)
-                        if homo_RTn is not None:
+                init_R_4 = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1,
+                    0], [0, 0, 0, 1]])
+                # while key != 'n':
+                    # for i in range(4):
+                print np.dot(homography.est_H, np.array([0, 0, 1]))
+                homography.est_H /= np.dot(homography.est_H, np.array([0,0,1]))[2]
+                homo_RTn = homography.get_pose_alt(prev_RT)
+                if homo_RTn is not None:
 
 
 #                             homo_R, homo_T, homo_norm = homo_RTn
@@ -126,55 +153,58 @@ if __name__ == '__main__':
 
 
 
-                            homo_R, homo_T, homo_norm = homo_RTn
-                            homo_T_trans = np.dot(np.dot(homo_R,
-                                init_R), homo_T)
-                            homo_RT = np.identity(4)
-                            homo_RT[0, 3] = homo_T_trans[0]
-                            homo_RT[1, 3] = homo_T_trans[1]
-                            homo_RT[2, 3] = homo_T_trans[2] + prev_RT[2, 3]
-                            homo_RT[0:3, 0:3] = homo_R[0:3, 0:3]
-                            homo_pos_pre = compose_pose(deepcopy(homo_RT))
-                            print homo_pos_pre.pose.orientation
-                            r, p, y = tf.transformations.euler_from_quaternion(np.array([homo_pos_pre.pose.orientation.x,
-                                homo_pos_pre.pose.orientation.y, homo_pos_pre.pose.orientation.z,
-                                homo_pos_pre.pose.orientation.w]))
-                            print r, p, y
-                            q = tf.transformations.quaternion_from_euler(-r, p,
-                                    y)
-                            homo_pos_pre.pose.orientation.x = q[0]
-                            homo_pos_pre.pose.orientation.y = q[1]
-                            homo_pos_pre.pose.orientation.z = q[2]
-                            homo_pos_pre.pose.orientation.w = q[3]
-                            print homo_pos_pre
-                            homo_R_flipped = Quaternion([homo_pos_pre.pose.orientation.w,
-                                homo_pos_pre.pose.orientation.x, homo_pos_pre.pose.orientation.y,
-                                homo_pos_pre.pose.orientation.z]).rotation_matrix
+                    homo_R, homo_T, homo_norm = homo_RTn
+                    homo_T_trans = np.dot(np.dot(homo_R,
+                        init_R), homo_T)
+                    homo_RT = np.identity(4)
+                    homo_RT[0:3,3] = (homo_T_trans.T + prev_RT[0:3,3])
+                    # homo_RT[0, 3] = homo_T_trans[0]
+                    # homo_RT[1, 3] = homo_T_trans[1]
+                    # homo_RT[2, 3] = homo_T_trans[2] + prev_RT[2, 3]
+                    homo_RT[0:3, 0:3] = homo_R[0:3, 0:3]
+                    homo_pos_pre = compose_pose(deepcopy(homo_RT))
 
-                            # homo_RT[0:3, 0:3] = np.dot(homo_R, prev_RT[0:3, 0:3])
-                            # homo_RT[0:3, 0:3] = np.dot(homo_R_flipped, prev_RT[0:3, 0:3])
-                            homo_RT[0:3, 0:3] = homo_R_flipped
+                    #homo_pos_pre = compose_pose(homo_RT.T)
+                    #homo_pos_pre = compose_pose(np.dot(homo_RT.T, init_R_4))
+                    #homo_pos_pre = compose_pose(np.dot(init_R_4, homo_RT))
 
-                            homo_pos = compose_pose(homo_RT)
-                            key = raw_input("press a key")
-                            if key == 'n':
-                                break
-                            elif key == 'p':
-                                print "new prev"
-                                prev_img = deepcopy(curr_img)
-                                break
-                            print key
-                            homopospub.publish(homo_pos)
-                            print "##############################"
-                            print i
-                            print homo_T
-                            print homo_norm
-                        else:
-                            print "##############################"
-                            print i
-                            print "could not find"
+                    r, p, y = tf.transformations.euler_from_quaternion(np.array([homo_pos_pre.pose.orientation.x,
+                        homo_pos_pre.pose.orientation.y, homo_pos_pre.pose.orientation.z,
+                        homo_pos_pre.pose.orientation.w]))
+
+                    q = tf.transformations.quaternion_from_euler(-r, p, y)
+                    #q = tf.transformations.quaternion_from_euler(r, p, y)
+                    homo_pos_pre.pose.orientation.x = q[0]
+                    homo_pos_pre.pose.orientation.y = q[1]
+                    homo_pos_pre.pose.orientation.z = q[2]
+                    homo_pos_pre.pose.orientation.w = q[3]
+                    homo_R_flipped = Quaternion([homo_pos_pre.pose.orientation.w,
+                        homo_pos_pre.pose.orientation.x, homo_pos_pre.pose.orientation.y,
+                        homo_pos_pre.pose.orientation.z]).rotation_matrix
+
+                    # homo_RT[0:3, 0:3] = np.dot(homo_R, prev_RT[0:3, 0:3])
+                    # homo_RT[0:3, 0:3] = np.dot(homo_R_flipped, prev_RT[0:3, 0:3])
+                    homo_RT[0:3, 0:3] = homo_R_flipped
+
+                    homo_pos = compose_pose(homo_RT)
+                    # key = raw_input("press a key")
+                    # if key == 'n':
+                    #     break
+                    # elif key == 'p':
+                    #     print "new prev"
+                    #     prev_img = deepcopy(curr_img)
+                    #     break
+                    # print key
+                    homopospub.publish(homo_pos)
+                    # print "##############################"
+                    # print homo_T
+                    # print homo_norm
+                else:
+                    print "##############################"
+                    print "could not find"
 
                 prepospub.publish(prev_pos)
                 postpospub.publish(curr_pos)
+                prev_img = deepcopy(curr_img) # Updates prev img
             else:
                 print "can't see aruco"
--- scripts/homography_class.py
+++ scripts/homography_class.py
@@ -4,6 +4,12 @@ from geometry_msgs.msg import PoseStamped
 import rospy
 import tf
 import copy
+from pyquaternion import Quaternion
+from camera_class import Camera
+import time
+from h2rPiCam import streamPi
+
+# camera = Camera(width=320, height=240)
 
 class Homography:
     """ Runs Homography AR"""
@@ -34,38 +40,44 @@ class Homography:
         self.search_params = search_params
         self.curr_pos = PoseStamped()
         self.curr_pos.header.frame_id = 'world'
-        self.est_R = None
-        self.est_t = None
+        self.est_RT = None
+        self.est_H = np.identity(3)
+        self.z_average = 1
 
     def pickHomography(self, Hs, ts, norms):
         sets = zip(Hs, ts, norms)
-        if len(sets) > 1:
-            last_two = [s for s in sets if s[2][2] > 0] # filter for norms with positive z
+        max_set = sets[0]
+        for s in sets:
+            if s[2][2] > max_set[1][1]: max_set = s
+        
+        return max_set
+        # if len(sets) > 1:
+        #     last_two = [s for s in sets if s[2][2] > 0] # filter for norms with positive z
 
-            # pick the winner with the lesser rotation
-            # where rotation magnitude is ||R - I||
-            if np.linalg.norm(last_two[0][0] - np.identity(3)) < np.linalg.norm(last_two[1][0] - np.identity(3)):
-                return last_two[0]
-            else:
-                return last_two[1]
-        else:
-            return sets[0]
+        #     # pick the winner with the lesser rotation
+        #     # where rotation magnitude is ||R - I||
+        #     if np.linalg.norm(last_two[0][0] - np.identity(3)) < np.linalg.norm(last_two[1][0] - np.identity(3)):
+        #         return last_two[0]
+        #     else:
+        #         return last_two[1]
+        # else:
+        #     return sets[0]
 
-    def updatePose(self):
-        big_mat = np.concatenate((self.est_R, self.est_t), 1)
-        big_mat = np.concatenate((big_mat,np.matrix([1,1,1,1])), 0)
-        quat_r = tf.transformations.quaternion_from_matrix(big_mat)
-        self.curr_pos.header.seq += 1
-        self.curr_pos.header.stamp = rospy.Time.now()
-        self.curr_pos.pose.position.x = self.est_t[0][0]
-        self.curr_pos.pose.position.y = self.est_t[1][0]
-        self.curr_pos.pose.position.z = self.est_t[2][0]
-        self.curr_pos.pose.orientation.x = quat_r[0]
-        self.curr_pos.pose.orientation.y = quat_r[1]
-        self.curr_pos.pose.orientation.z = quat_r[2]
-        self.curr_pos.pose.orientation.w = quat_r[3]
+    # def updatePose(self):
+    #     big_mat = np.concatenate((self.est_R, self.est_t), 1)
+    #     big_mat = np.concatenate((big_mat,np.matrix([1,1,1,1])), 0)
+    #     quat_r = tf.transformations.quaternion_from_matrix(big_mat)
+    #     self.curr_pos.header.seq += 1
+    #     self.curr_pos.header.stamp = rospy.Time.now()
+    #     self.curr_pos.pose.position.x = self.est_t[0][0]
+    #     self.curr_pos.pose.position.y = self.est_t[1][0]
+    #     self.curr_pos.pose.position.z = self.est_t[2][0]
+    #     self.curr_pos.pose.orientation.x = quat_r[0]
+    #     self.curr_pos.pose.orientation.y = quat_r[1]
+    #     self.curr_pos.pose.orientation.z = quat_r[2]
+    #     self.curr_pos.pose.orientation.w = quat_r[3]
 
-        return self.curr_pos
+    #     return self.curr_pos
 
 # Helper for findAruco
     def detectArucoMarker(self, img):
@@ -92,17 +104,42 @@ class Homography:
             self.updatePose()
             return True
 
+    def updateHNew(self, curr_img, prev_img):
+        orb = cv2.ORB_create(nfeatures=500, nlevels=8, scaleFactor=2)
+        kp1, des1 = orb.detectAndCompute(prev_img,None)
+        kp2, des2 = orb.detectAndCompute(curr_img,None)
+
+        good = []
+        flann = cv2.FlannBasedMatcher(self.index_params, self.search_params)
+
+        if des1 is not None and des2 is not None and len(des1) > 3 and len(des2) > 3:
+            matches = flann.knnMatch(des1,des2,k=2)
+            # store all the good matches as per Lowe's ratio test.
+            for test in matches:
+                if len(test) > 1:
+                    m, n = test
+                    if m.distance < 0.7*n.distance:
+                        good.append(m)
+
+            if len(good) > self.min_match_count:
+                src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+                dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+
+                H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+                # self.est_H = H
+                self.est_H = np.dot(self.est_H, H)
+
 
 # Uses homography to get the R and t
-    def getRt(self, img2, img1 = None):
-        assert img1 is not None or (self.kp1 is not None and self.des1 is not None)
+    def updateH(self, curr_img, prev_img = None):
+        assert prev_img is not None or (self.kp1 is not None and self.des1 is not None)
 
         # Initiate SURF detector
-        orb = cv2.ORB_create()
+        orb = cv2.ORB_create(nfeatures=500, nlevels=8, scaleFactor=2)
         # find the keypoints and descriptors with SIFT
-        if self.kp1 is None:
-            self.kp1, self.des1 = orb.detectAndCompute(img1,None)
-        kp2, des2 = orb.detectAndCompute(img2,None)
+        # if self.kp1 is None:
+        self.kp1, self.des1 = orb.detectAndCompute(prev_img,None)
+        kp2, des2 = orb.detectAndCompute(curr_img,None)
 
         # Match keypoints
         good = []
@@ -122,41 +159,250 @@ class Homography:
 
             # Find Homography
             H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-            retval, Rs, ts, norms = cv2.decomposeHomographyMat(H, self.camera_matrix)
-
-            R, t, norm = self.pickHomography(Rs, ts, norms)
-            t[0] *= -1
-            R[2][0] *= -1
-            R[0][2] *= -1
-            self.kp1 = kp2
-            self.des1 = des2
-            return R, t
+            self.est_H = H
+            # self.est_H = np.dot(H, self.est_H)
+            # retval, Rs, ts, norms = cv2.decomposeHomographyMat(H, self.camera_matrix)
+            # FIRST FRAME
+            # R, t, norm = self.pickHomography(Rs, ts, norms)
+            # t[0] *= -1
+            # z_sum = 0
+            # z_num = 0
+            # for kp in src_pts:
+            #     z_sum +=  np.dot(self.est_H,
+            #             np.concatenate((kp,np.array([[1]])),1).T)[2]
+            #     z_num += 1
+            # self.z_average = z_sum / z_num
+            # print self.z_average
+            # self.est_H = self.est_H / z_average
+            # R[2][0] *= -1
+            # R[0][2] *= -1
+            # for kp in src_pts:
+                # print np.dot(self.est_H,
+                #         np.concatenate((kp,np.array([[1]])),1).T)
+            # FIRST FRAME
+            # self.kp1 = kp2
+            # self.des1 = des2
+            return True
 
         else:
             print "Not enough matches are found - %d/%d" % (len(good),self.min_match_count)
-            return None
+            return False
 
 # Helps updatePose
-    def move(self, R0, t0, R, t):
-        R_tmp = copy.deepcopy(R0)
-        R_tmp[0][2] *= -1 # fix roll problem
-        R_tmp[2][0] *= -1
-        R_tmp[1][0] *= -1 # fix yaw problem
-        R_tmp[0][1] *= -1
-        t1 = t0 + np.dot(R_tmp.T, t)*t0[2]
-        R1 = np.dot(R ,R0)
-        return (R1, t1)
+    # def move(self, R0, t0, R, t):
+    #     # R_tmp = copy.deepcopy(R0)
+    #     # R[0][2] *= -1 # fix roll problem
+    #     # R[2][0] *= -1
+    #     # R[1][2] *= -1 # fix pitch problem
+    #     # R[2][1] *= -1
+    #     # print R0
+    #     # print t0
+    #     # print R
+    #     # print t
+    #     # t1 = t0 + np.dot(R0.T, t)*t0[2]
+    #     # t1 = t0 + t*t0[2]
+    #     R1 = np.dot(R0 ,R)
+    #     t1 = t0 + np.dot(R0, t)*t0[2]
+    #     # R1 = R0
+    #     return (R1, t1)
 
 # Updates curr pos based on other params
-    def updatePos(self, new_img, curr_img = None):
-        Rt = self.getRt(new_img, curr_img)
-        if Rt is not None:
-            R, t = Rt
-            self.est_R, self.est_t = self.move(self.est_R, self.est_t, R,t)
-            # convert from R, t to quaternion
-            big_mat = np.concatenate((self.est_R, self.est_t), 1)
-            big_mat = np.concatenate((big_mat,np.matrix([1,1,1,1])), 0)
-            quat_r = tf.transformations.quaternion_from_matrix(big_mat)
-            return self.updatePose()
-        else:
-            return None
+    # def updatePos(self, curr_img, prev_img = None):
+    #     Rt = self.updateH(curr_img, prev_img)
+    #     if Rt is not None:
+    #         R, t = Rt
+    #         self.est_R, self.est_t = self.move(self.est_R, self.est_t, R,t)
+    #         return True
+    #     else:
+    #         return None
+    
+    def compose_pose(self, RT):
+        pos = PoseStamped()
+        quat_r = tf.transformations.quaternion_from_matrix(RT)
+        pos.header.frame_id = 'world'
+        pos.header.seq += 1
+        pos.header.stamp = rospy.Time.now()
+        pos.pose.position.x = RT[0][3]
+        pos.pose.position.y = RT[1][3]
+        pos.pose.position.z = RT[2][3]
+        pos.pose.orientation.x = quat_r[0]
+        pos.pose.orientation.y = quat_r[1]
+        pos.pose.orientation.z = quat_r[2]
+        pos.pose.orientation.w = quat_r[3]
+
+        return pos
+ 
+    def get_pose_alt(self, start_RT):
+        retval, Rs, ts, norms = cv2.decomposeHomographyMat(self.est_H \
+                /self.z_average, self.camera_matrix)
+        # print 'Weve got {} options'.format(len(Rs))
+        # if s < len(Rs):
+        min_index = 0
+        min_z_norm = 0
+        for i in range(len(Rs)):
+            if norms[i][2] < min_z_norm:
+                min_z_norm = norms[i][2]
+                min_index = i
+
+        T = np.zeros(3)
+        T = ts[min_index] * start_RT[2,3]
+        # print 'height', start_RT[2, 3]
+        # T[0] = ts[s][0] * start_RT[2, 3]
+        # T[1] = ts[s][1] * start_RT[2, 3]
+        # T[2] = ts[s][2] * start_RT[2, 3]
+        
+        return Rs[min_index], T, norms[min_index]
+        
+        print 'ROTATION MAGNITUDE', np.linalg.norm(R[0:3,0:3]-np.identity(3))
+        twiddle = np.array([[1,0,0],[0,-1,0],[0,0, -1]])
+        start_T = start_RT[0:3,3]
+        start_R = start_RT[0:3,0:3]
+        start_R_inv = start_R.T
+        final_T = start_T + np.dot(start_R_inv, -1 * T)
+        R_twiddled = np.dot(start_R_inv, twiddle)
+        
+        # final_R = np.dot(np.dot(start_R, twiddle), R[0:3,0:3])
+        final_R = R_twiddled
+        RT = np.identity(4)
+        RT[0:3,3] = final_T
+        RT[0:3,0:3] = final_R
+        # flip_around_x = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        # T_flipped = np.dot(flip_around_x, T)
+        # RT = np.dot(R, T)
+        # flip_RT = np.dot(start_RT, RT)
+        
+        # RT = np.dot(R, T)
+        # flip_around_x = np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        # RT_offset = np.dot(RT, flip_around_x)
+        # flip_RT = np.dot(start_RT, RT_offset)
+     
+        #flip_RT = np.dot(RT_offset, start_RT)
+        # new_R = np.array([[flip_RT[i, j] for j in range(3)] for i in
+        #     range(3)])
+        # flip_new_R = np.dot(new_R, np.array([[1, 0, 0], [0, -1, 0], [0, 0,
+        #     -1]]))
+        # for i in range(3):
+        #     for j in range(3):
+        #         flip_RT[i, j] = flip_new_R[i, j]
+
+        # raw_input('press any key (but n) to advance')
+        print s
+        print 'RT\n', RT
+        print 'norm\n', norm
+
+        pos = self.compose_pose(RT)
+        return pos
+        # else:
+        #     print 'RAN OUT OF AXES'
+        #     # RT, norm = self.pickHomography(Rs, ts, norms)
+        #     return None
+
+
+
+         
+        # return pos
+
+
+    # def get_pose(self, start_R, start_t, s):
+    #     # print self.est_H
+    #     retval, Rs, ts, norms = \
+    #     cv2.decomposeHomographyMat(self.est_H, self.camera_matrix)
+    #     # print 'Weve got {} options'.format(len(Rs))
+    #     if s < len(Rs):
+    #         R = Rs[s]
+    #         t = ts[s]
+    #         norm = norms[s]
+    #     else:
+    #         print 'RAN OUT OF AXES'
+    #         R, t, norm = self.pickHomography(Rs, ts, norms)
+    #     # if np.absolute(norm[2]) < np.linalg.norm(norm[0:2]): 
+    #         # print 'NOT USING THIS ONE'
+    #     # t[0] *= -1 
+    #     # t[1] *= -1
+    #     # t[2] *= -1
+    #     # R_tmp = copy.deepcopy(R)
+    #     # R_tmp[0][1] *= -1
+    #     # R_tmp[1][0] *= -1
+    #     # R_tmp[0][2] *= -1
+    #     # R_tmp[2][0] *= -1
+    #     # R_tmp[1][2] *= -1
+    #     # R_tmp[2][1] *= -1
+   
+    #     # print 'ANGLE', np.linalg.norm(R - np.identity(3)),'MAGNITUDE',\
+    #     # np.linalg.norm(t), 'NORM', norm.T
+    #     # print 'ROTATION\n', R
+    #     # print 'TRANSLATION\n', t
+        
+    #     flip_around_x = np.array([[1, 0, 0], [0,-1,0], [0,0,-1]])
+    #     # flipped_R = np.dot(flip_around_x, R)
+    #     # R[1][2] *= -1
+    #     # R[2][1] *= -1
+    #     # R[0][1] *= -1
+    #     # R[1][0] *= -1
+    #     flip_R = np.dot(RT, flip_around_x)
+    #     # t = np.dot(flip_around_x, t)
+    #     new_R = np.dot(start_R, R)
+    #     diff_t = np.dot(new_R.T, t)*start_t[2]
+    #     diff_t[2] *= -1
+    #     new_t = start_t +  diff_t       
+    #     pos = self.compose_pose(new_R, new_t)
+    #     return pos
+
+
+first_pos = None
+
+def seed_pos(data):
+    global first_pos
+    if first_pos is None:
+        first_pos = data
+
+
+def decompose_pose(p):
+    q = Quaternion(p.pose.orientation.w, p.pose.orientation.x,
+        p.pose.orientation.y, p.pose.orientation.z)
+    T = np.identity(4)
+    T[0, 3] = p.pose.position.x
+    T[1, 3] = p.pose.position.y
+    T[2, 3] = p.pose.position.z
+    R = q.transformation_matrix
+    return np.dot(R, T)
+
+if __name__ == '__main__':
+    rospy.init_node("homography_class")
+    pospub = []
+    pospub.append(rospy.Publisher('/pidrone/homo_est1', PoseStamped,
+        queue_size=1))
+    pospub.append(rospy.Publisher('/pidrone/homo_est2', PoseStamped,
+        queue_size=1))
+    pospub.append(rospy.Publisher('/pidrone/homo_est3', PoseStamped,
+        queue_size=1))
+    pospub.append(rospy.Publisher('/pidrone/homo_est4', PoseStamped,
+        queue_size=1))
+    rospy.Subscriber("/pidrone/est_pos", PoseStamped, seed_pos)
+    homography = Homography()
+    prev_img = streamPi().next()
+    position_flag = False
+    position = None
+    first_img = None
+    global first_pos
+    print 'Waiting for first position from optitrak'
+    while first_pos is None:
+        time.sleep(0.001)
+    print "found first pos"
+    homography.est_RT = decompose_pose(first_pos) 
+    start_RT = decompose_pose(first_pos)
+    print "decomposed"
+    # cv2.namedWindow('preview')
+    for curr_img in streamPi():
+        if first_img is None: first_img = curr_img
+        elif first_img is not None and not position_flag:
+            print 'Starting homography calculations'
+            position_flag = homography.updateH(curr_img, prev_img = first_img)   
+        elif position_flag:
+            homography.updateH(curr_img)
+            for i in range(4):
+                raw_input('press key to continue')
+                position = homography.get_pose_alt(start_RT, i)
+                print "################"
+                if position is not None:
+                    pospub[0].publish(position)
