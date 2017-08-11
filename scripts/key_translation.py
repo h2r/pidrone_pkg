11cbb4a2440e6b5e2e0550457d11a688a55c98d5
Stefanie Tellex <stefie10@cs.brown.edu>
2017-08-11 07:23:20 +0000

    basic key arm and disarm

--- scripts/key_translation.py
+++ scripts/key_translation.py
@@ -1,7 +1,6 @@
 import rospy
 from pidrone_pkg.msg import Mode
-
-
+import getch
 
 
 
@@ -9,35 +8,56 @@ def main():
     rospy.init_node("key_translation")
     import sys, tty, termios
     fd = sys.stdin.fileno()
-    tty.setraw(sys.stdin.fileno())
+    #attr = termios.tcgetattr(sys.stdin.fileno())
+    #tty.setraw(sys.stdin.fileno())
     mode = Mode()
     mode.mode = 4
     modepub = rospy.Publisher('/pidrone/set_mode', Mode, queue_size=1)
-
     rate = rospy.Rate(10)
-    
-    while not rospy.is_shutdown(): 
-        ch = sys.stdin.read(1)
-        if ch == "j":
-            mode.mode = 4
-        elif ch == "k":
-            mode.mode = 0
+    msg = """
+Commands: 
+j:  arm
+k:  disarm
+t:  takeoff
+q:  quit
+"""
+    try:
+        while not rospy.is_shutdown():
+            print msg
+            ch = getch.getch()
+            if ch == "k":
+                mode.mode = 4
+            elif ch == "j":
+                mode.mode = 0
+            elif ch == "a":
+                mode.mode = 5
+                mode.x_velocity = 0
+                mode.y_velocity = 0
+                mode.z_velocity = 0
+            elif ch == "d":
+                mode.mode = 5
+                mode.x_velocity = 0
+                mode.y_velocity = 0
+                mode.z_velocity = 0
+            elif ch == "t":
+                mode.mode = 2
+            elif ch == "q":
+                break
+            elif ord(ch) == 3:
+                break
+            else:
+                print "unknown character: '%d'" % ord(ch)
+                pass
 
-        elif ch == "a":
-            mode.mode = 5
-            mode.x_velocity = 10
-        elif ch == "d":
-            mode.mode = 5
-            mode.x_velocity = -10
-        else:
-            pass
-        
+            modepub.publish(mode)
+            rate.sleep()
+
+    finally:
+        mode.mode = 4
         modepub.publish(mode)
         rate.sleep()
 
-
+    modepub.publish(mode)
         
-
-                                                    
 if __name__ == "__main__":
     main()
