1d7b6d53a03243fe5bf3b4bf2b306a7b2146f4d0
Stefanie Tellex <stefie10@cs.brown.edu>
2017-09-05 01:49:33 +0000

    comments and debug output

--- scripts/joy_translation.py
+++ scripts/joy_translation.py
@@ -23,7 +23,7 @@ def joy_callback(data):
     global z_counter
     global z_step
     global z_total_steps
-
+    print "callback"
     if data.buttons[3] == 1:
         z_counter = (z_counter+1) % z_total_steps
         print 3, "Z Stepping", z_counter
@@ -34,32 +34,35 @@ def joy_callback(data):
             mode.z_velocity = -z_step
         else:
             mode.z_velocity = z_step
-        print mode
+        print "mode", mode
         modepub.publish(mode)
     if data.buttons[7] == 1:
-        print 7
-        print mode
+        print "button", 7
+        print "mode", mode
     if data.buttons[8] == 1:
-        print 8
-        print mode
+        print "botton", 8
+        print "mode", mode
     if data.buttons[9] == 1:
-        print 9
-        print mode
+        print "button", 9
+        print "mode", mode
     if data.buttons[10] == 1:
-        print 10
-        print mode
+        print "button", 10
+        print "mode", mode
 
     if data.buttons[4] == 1:
+        # disarm
         mode.mode = 4
-        print mode
+        print "mode", mode
         modepub.publish(mode)
     elif data.buttons[6] == 1:
+        # land
         mode.mode = 3
-        print mode
+        print "mode", mode
         modepub.publish(mode)
     elif data.buttons[5] == 1:
+        # arm
         mode.mode = 0
-        print mode
+        print "mode", mode
         modepub.publish(mode)
     elif data.buttons[7] == 1:
         mode.mode = 5
@@ -67,11 +70,12 @@ def joy_callback(data):
         mode.y_velocity = data.axes[3] * scalar
         mode.z_velocity = data.axes[1]
         mode.yaw_velocity = -200.0 * data.axes[0]
-        print mode
+        print "mode", mode
         modepub.publish(mode)
     elif data.buttons[0] == 1:
+        # takeoff
         mode.mode = 2
-        print mode
+        print "mode", mode
         modepub.publish(mode)
 
     # should be able to do these at the same time 
