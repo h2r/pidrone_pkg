db5975fd63a887b782896e26263d2cf30479a0cf
Stefanie Tellex <stefie10@cs.brown.edu>
2017-09-14 02:38:34 +0000

    Added script to change from AP to RLAB

---
+++ connect_to_rlab.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == "__main__":
+# Stopping Services
+    os.system("sudo systemctl stop hostapd")
+
+    os.system("sudo systemctl stop isc-dhcp-server.service")
+    os.system("sudo ifdown wlan0")
+# Starting wpa_supplicant
+    os.system("sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf")
+    os.system("sudo ifup wlan0=rlab_wifi")
