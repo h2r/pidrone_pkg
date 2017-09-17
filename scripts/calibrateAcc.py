eabd8ad867ff98a68769c4a0563f4b447fb2cb91
Stefanie Tellex <stefie10@cs.brown.edu>
2017-09-17 22:09:50 +0000

    script to calibrte acc

---
+++ scripts/calibrateAcc.py
@@ -0,0 +1,14 @@
+from h2rMultiWii import MultiWii
+import time
+
+def main():
+    board = MultiWii("/dev/ttyUSB0")
+    print "Calibrate ACC... make sure we are level and still."
+    time.sleep(1)
+    board.sendCMD(0, MultiWii.ACC_CALIBRATION, [])
+    board.receiveDataPacket()
+    time.sleep(2)
+    print "Done!"
+    
+if __name__ == "__main__":
+    main()
