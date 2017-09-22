d5d2fc4d1a1981682c1101b3f7aceac5468b1b56
Stefanie Tellex <stefie10@cs.brown.edu>
2017-09-22 17:58:16 +0000

    fix bug

--- scripts/checkbat.py
+++ scripts/checkbat.py
@@ -1,10 +1,13 @@
 from h2rMultiWii import MultiWii as MW
 
-def voltage(board):
+def voltage(data):
     return float(data['vbat'])/10.0
 
-if __name__ == '__main__':
+def main():
     board = MW('/dev/ttyUSB0')
     data = board.getData(MW.ANALOG)
-    print 'BATTERY VOLTAGE IS\t{}v'.format(voltage(board))
+    print 'BATTERY VOLTAGE IS\t{}v'.format(voltage(data))
     board.close()
+
+if __name__ == '__main__':
+    main()
