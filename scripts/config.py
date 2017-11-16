0e9a3eb15002f671c2437d24b3cfb721abe2c2c7
IzzyBrand <isaiah_brand@brown.edu>
2017-11-16 15:59:20 -0500

    untest MVP for skyline_controller

---
+++ scripts/config.py
@@ -0,0 +1,12 @@
+#######################################################################
+##	GENERAL
+#######################################################################
+SKYLINE_ENABLED = True
+
+#######################################################################
+##	TOPICS
+#######################################################################
+SKYLINE_COMMAND_TOPIC 		= '/pidrone/skyline/cmd'
+SKYLINE_STATUS_TOPIC 		= '/pidrone/skyline/status'
+INFRARED_TOPIC 				= '/pidrone/infrared'
+JAVASCRIPT_COMMAND_TOPIC 	= '/pidrone/command/javascript'
