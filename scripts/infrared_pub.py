c94ee20fc5bfc40b71b6d8b89b4e8eb627ad614d
baichuan <baichuan55555@gmail.com>
2018-03-23 15:27:33 -0400

    delete unnecessary codes

--- scripts/infrared_pub.py
+++ scripts/infrared_pub.py
@@ -1,5 +1,4 @@
 import rospy
-import time
 from sensor_msgs.msg import Range
 import Adafruit_ADS1x15
 
@@ -12,45 +11,39 @@ m = 181818.18181818182 * 1.238 # 1.3 / 1.05
 b = -8.3 + 7.5
 #b = -8.3 + 7.5 - 17.0
 smoothed_distance = 0
-alpha = 0.7
+alpha = 0.2
 
-alpha_50 = 0.2
-alpha_20 = 0.2
 
 def get_range():
     global smoothed_distance
 
-    slew_distance = max( min( smoothed_distance, 50.0 ), 20.0)
-    alpha = ( (slew_distance - 20.0) * alpha_50 + (50.0 - slew_distance) * alpha_20 ) / (50.0-20.0)
-    #print alpha
-    #alpha = 1. # TESTING FOR COMPARING ULTRA AND INFRA
-
     voltage = adc.read_adc(0, gain=GAIN)
-    #voltage = 0
     if voltage <= 0:
         voltage = 1
         print "ERROR: BAD VOLTAGE!!!"
-    distance = (1.0 / voltage) * m + b
+    distance = ((1.0 / voltage) * m + b) / 100.0
     smoothed_distance = (1.0 - alpha) * smoothed_distance + alpha * distance
-    smoothed_distance = min(smoothed_distance, 55.0)
+    smoothed_distance = min(smoothed_distance, 0.55)
 
     return smoothed_distance
+
 
 def main():
     rospy.init_node("infrared_pub")
     pub = rospy.Publisher('/pidrone/infrared', Range, queue_size=1)
     rnge = Range()
-    rnge.max_range = 100 * 0.01
+    rnge.max_range = 0.8
     rnge.min_range = 0
     rnge.header.frame_id = "base"
-    r = rospy.Rate(60)
+    r = rospy.Rate(100)
     print "publishing IR"
     while not rospy.is_shutdown():
         rnge.header.stamp = rospy.get_rostime()
         rnge.header.frame_id = "ir_link"
-        rnge.range = get_range() * 0.01
+        rnge.range = get_range()
         pub.publish(rnge)
         r.sleep()
+
 
 if __name__ == "__main__":
     main()
