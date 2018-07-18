34f769efb94f05493f215b7f37eab66ae038db6b
Garrett Warren <garrettwarren@garretts-mbp.rlab.cs.brown.edu>
2018-07-18 13:07:27 -0400

    refactored this to a class

--- scripts/infrared_pub.py
+++ scripts/infrared_pub.py
@@ -1,59 +1,89 @@
 import rospy
 from sensor_msgs.msg import Range
 import Adafruit_ADS1x15
-
-adc = Adafruit_ADS1x15.ADS1115()
-GAIN = 1
-# XXX jgo these values underestimate the height on my sensor significantly
-#m = 181818.18181818182
-#m = 2.0 * 181818.18181818182
-m = 181818.18181818182 * 1.238 # 1.3 / 1.05
-b = -8.3 + 7.5
-#b = -8.3 + 7.5 - 17.0
-smoothed_distance = 0
-distance = 0
-alpha = 0.2
+import signal
+import sys
 
 
-def get_range():
-    global smoothed_distance
-    global distance
+class IR(object):
+    """A class that reads, analyzes, and publishes IR sensor data. The raw data
+    is published on the /raw-infrared topic (this will be used by the
+    state_estimation node), and the smoothed data is published on the /infrared
+    topic (this is the data currently used by sensors and controllers).
 
-    voltage = adc.read_adc(0, gain=GAIN)
-    if voltage <= 0:
-        voltage = 1
-        print "ERROR: BAD VOLTAGE!!!"
-    distance = ((1.0 / voltage) * m + b) / 100.0
-    smoothed_distance = (1.0 - alpha) * smoothed_distance + alpha * distance
-    smoothed_distance = min(smoothed_distance, 0.55)
+    Publisher:
+    /pidrone/infrared
+    /pidrone/raw_infrared
+    """
 
-    return distance, smoothed_distance
+    def __init__(self):
+        self.adc = Adafruit_ADS1x15.ADS1115()
+        self.GAIN = 1
+        self.distance = 0
+        self.smoothed_distance = 0
+        # Smoothing values
+        # XXX jgo these values underestimate the height on my sensor significantly
+        #m = 181818.18181818182
+        #m = 2.0 * 181818.18181818182
+        #b = -8.3 + 7.5 - 17.0
+        self.alpha = 0.2
+        self.m = 181818.18181818182 * 1.238 # 1.3 / 1.05
+        self.b = -8.3 + 7.5
 
+    def get_range(self):
+        """Read the data from the adc and update the distance and
+        smoothed_distance values."""
+        voltage = self.adc.read_adc(0, self.GAIN)
+        if voltage <= 0:
+            voltage = 1
+            print "ERROR: BAD VOLTAGE!!!"
+        self.distance = ((1.0 / voltage) * self.m + self.b) / 100.0
+        self.smoothed_distance = (1.0 - self.alpha) * self.smoothed_distance + self.alpha * self.distance
+        self.smoothed_distance = min(self.smoothed_distance, 0.55)
+
+    def publish_range(self, range, publisher):
+        """Create and publish the Range message to publisher."""
+        msg = Range()
+        msg.header.stamp = rospy.get_rostime()
+        msg.max_range = 0.8
+        msg.min_range = 0
+        msg.range = range
+        msg.header.frame_id = "base"
+        publisher.publish(msg)
+
+    def ctrl_c_handler(self, signal, frame):
+        """Gracefully quit the infrared_pub node"""
+        print "\nCaught ctrl-c! Stopping node."
+        sys.exit()
 
 def main():
-    rospy.init_node("infrared_pub")
-    pub = rospy.Publisher('/pidrone/infrared', Range, queue_size=1)
-    rnge = Range()
-    rnge.max_range = 0.8
-    rnge.min_range = 0
-    rnge.header.frame_id = "base"
-    rawpub = rospy.Publisher('/pidrone/raw_infrared', Range, queue_size=1)
-    rawrnge = Range()
-    rawrnge.max_range = 0.8
-    rawrnge.min_range = 0
-    rawrnge.header.frame_id = "base"
-    r = rospy.Rate(100)
-    print "publishing IR"
-    while not rospy.is_shutdown():
-        rnge.header.stamp = rospy.get_rostime()
-        rnge.header.frame_id = "ir_link"
-        rawrnge.range, rnge.range = get_range()
-        pub.publish(rnge)
-        rawrnge.header.stamp = rospy.get_rostime()
-        rawrnge.header.frame_id = "ir_link"
-        rawpub.publish(rawrnge)
-        r.sleep()
+    """Start the ROS node, create the publishers, and continuosly update and
+    publish the IR sensor data"""
 
+    # ROS Setup
+    ###########
+    rospy.init_node("infrared_pub")
+
+    # Publishers
+    ############
+    smoothedpub = rospy.Publisher('/pidrone/infrared', Range, queue_size=1)
+    rawpub = rospy.Publisher('/pidrone/raw_infrared', Range, queue_size=1)
+    print 'Publishing IR'
+
+    # Non-ROS Setup
+    ###############
+    # create IR object
+    ir = IR()
+    # set the while loop frequency
+    r = rospy.Rate(100)
+    # set up the ctrl-c handler
+    signal.signal(signal.SIGINT, ir.ctrl_c_handler)
+
+    while not rospy.is_shutdown():
+        ir.get_range()
+        ir.publish_range(ir.smoothed_distance, smoothedpub)
+        ir.publish_range(ir.distance, rawpub)
+        r.sleep()
 
 if __name__ == "__main__":
     main()
