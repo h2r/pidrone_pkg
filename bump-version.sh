bbd5b93fa5068bfbfc23a6952bc044fb0b0c8ede
Sophie Yang <sophie_yang@brown.edu>
2018-07-26 10:50:22 -0400

    Added bump-version.sh script

---
+++ bump-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# works with a file called VERSION in the current directory,
+# the contents of which should be a semantic version number
+# such as "1.2.3"
+
+# this script will display the current version, automatically
+# suggest a "minor" version update, and ask for input to use
+# the suggestion, or a newly entered value.
+
+# once the new version number is determined, the script will
+# pull a list of changes from git history, prepend this to
+# a file called CHANGES (under the title of the new version
+# number).
+
+if [ -f VERSION ]; then
+    BASE_STRING=`cat VERSION`
+    BASE_LIST=(`echo $BASE_STRING | tr '.' ' '`)
+    V_MAJOR=${BASE_LIST[0]}
+    V_MINOR=${BASE_LIST[1]}
+    echo "Current version : $BASE_STRING"
+    V_MAJOR=$((V_MAJOR + 1))
+    SUGGESTED_VERSION="$V_MAJOR.$V_MINOR"
+    read -p "Enter a version number [$SUGGESTED_VERSION]: " INPUT_STRING
+    if [ "$INPUT_STRING" = "" ]; then
+        INPUT_STRING=$SUGGESTED_VERSION
+    fi
+    echo "Will set new version to be $INPUT_STRING"
+    echo $INPUT_STRING > VERSION
+    echo "Version $INPUT_STRING:" > tmpfile
+    git log --pretty=format:" - %s" "v$BASE_STRING"...HEAD >> tmpfile
+    echo "" >> tmpfile
+    echo "" >> tmpfile
+    cat CHANGES >> tmpfile
+    mv tmpfile CHANGES
+    git add CHANGES VERSION
+    git commit -m "Version bump to $INPUT_STRING"
+else
+    echo "Could not find a VERSION file"
+fi
