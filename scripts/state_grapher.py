96c85590f011f55b386600685db18cd50b9bd816
Theo Guerin <theo_guerin@brown.edu>
2018-07-30 14:38:46 -0400

    Using roslibpy for state_grapher.py

--- scripts/state_grapher.py
+++ scripts/state_grapher.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 
 # ROS imports
-import rospy
-from sensor_msgs.msg import Imu, Range
-from pidrone_pkg.msg import State
-from geometry_msgs.msg import TwistStamped
-from std_msgs.msg import Header
+# import rospy
+# from sensor_msgs.msg import Imu, Range
+# from pidrone_pkg.msg import State
+# from geometry_msgs.msg import TwistStamped
+# from std_msgs.msg import Header
+import roslibpy
 
 # Other imports
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from Queue import Queue
-
+ 
 
 class StateGrapher(object):
     
-    def __init__(self):
-        # Initialize Queue objects to allow plotting to occur in main thread
-        self.state_queue = Queue()
-        self.ir_data_queue = Queue()
+    def __init__(self, hostname):
+        self.hostname = hostname
         self.setup_plots()
         
         # Lists to store data for plotting
@@ -33,30 +31,40 @@ class StateGrapher(object):
         
     def initialize_ros(self):
         '''
-        Initialize ROS-related objects, e.g., the node, subscribers, etc.
+        Initialize ROS-related objects, e.g., the connection to the ROS master,
+        the subscribers, etc.
         '''
         self.node_name = 'state_grapher'
-        print 'Initializing {} node...'.format(self.node_name)
-        rospy.init_node(self.node_name)
+        print 'Initializing {} connection to ROS master running on {}...'.format(
+                self.node_name, self.hostname)
+        self.ros = roslibpy.Ros(host=self.hostname, port=9090)
         
         # To be able to plot state estimates:
-        rospy.Subscriber('/pidrone/state', State, self.state_callback)
+        self.state_sub = roslibpy.Topic(ros=self.ros, name='/pidrone/state', message_type='pidrone_pkg/State')
         
         # To be able to plot raw sensor data:
-        rospy.Subscriber('/pidrone/infrared', Range, self.ir_data_callback) # TODO: infrared or raw_infrared?
+        self.ir_sub = roslibpy.Topic(ros=self.ros, name='/pidrone/infrared', message_type='sensor_msgs/Range') # TODO: infrared or raw_infrared?
         # TODO: Later, add Imu and TwistStamped (optical flow) data support
         
+    def start_subscribers(self):
+        print 'Starting subscribers...'
+        self.state_sub.subscribe(self.state_callback)
+        self.ir_sub.subscribe(self.ir_data_callback)
+        print 'Subscribers started'
+        
+    def state_callback(self, data):
+        print 'Got a State message'
+        self.update_plot_state(data)
+        
+    def ir_data_callback(self, data):
+        #print 'Got a Range message'
+        self.update_plot_ir(data)
+    
     def setup_plots(self):
         plt.ion()
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
-        self.time_range = 10 # seconds to show on the x-axis
-        
-    def state_callback(self, data):
-        self.state_queue.put(data)
-        
-    def ir_data_callback(self, data):
-        self.ir_data_queue.put(data)
+        self.time_range = 5 # seconds to show on the x-axis
         
     def update_plot_state(self, msg):
         '''
@@ -86,8 +94,8 @@ class StateGrapher(object):
         '''
         Update the plot(s) with IR data.
         '''
-        secs = msg.header.stamp.secs
-        nsecs = msg.header.stamp.nsecs
+        secs = msg['header']['stamp']['secs']
+        nsecs = msg['header']['stamp']['nsecs']
         new_time = secs + nsecs*1e-9
         if self.ir_times:
             # If we already have data, check if we should delete old data
@@ -96,39 +104,43 @@ class StateGrapher(object):
                 del self.ir_times[0]
                 del self.ir_data[0]
         self.ir_times.append(new_time)
-        z_pos = msg.range
+        z_pos = msg['range']
         self.ir_data.append(z_pos)
+        print new_time
         
         if len(self.ir_times) == 1:
             x_max = self.ir_times[0] + self.time_range
+            do_plot = True
         else:
             x_max = new_time
-        self.update_plot(self.ir_times[0], x_max)
+            # Don't update the plot that often, as it takes a fair amount of
+            # time to do so
+            if (new_time - self.last_plot_time) > 0.5:
+                do_plot = True
+            else:
+                do_plot = False
+                
+        if do_plot:
+            self.last_plot_time = new_time
+            self.update_plot(self.ir_times[0], x_max)
         
     def update_plot(self, x_min, x_max):
         print 'Updating plot'
         # Plot the data
-        self.ax.plot(self.ir_times, self.ir_data)
-        self.ax.plot(self.state_times, self.state_data)
+        self.ax.plot(self.ir_times, self.ir_data, color='red')
+        self.ax.plot(self.state_times, self.state_data, color='blue')
         self.ax.set_xlim(x_min, x_max)
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
         #self.fig.canvas.draw_idle()
         
     def run(self):
-        r = rospy.Rate(30) # Hz
-        while not rospy.is_shutdown():
-            if not self.state_queue.empty():
-                # Update plot with state data
-                self.update_plot_state(self.state_queue.get())
-            if not self.ir_data_queue.empty():
-                # Update plot with raw IR data
-                self.update_plot_ir(self.ir_data_queue.get())
-            r.sleep()
+        self.ros.on_ready(self.start_subscribers, run_in_thread=True)
+        self.ros.run_forever()
         
         
 def main():
-    sg = StateGrapher()
+    sg = StateGrapher(hostname='tdrone-blue')
     try:
         # Wait until node is halted
         #rospy.spin()
