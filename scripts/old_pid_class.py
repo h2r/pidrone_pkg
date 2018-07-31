e7967a35ded06a731293257f3503c8ef367ecd12
Garrett Warren <garrettwarren@garretts-mbp.rlab.cs.brown.edu>
2018-07-31 10:45:40 -0400

    renamed this version

---
+++ scripts/old_pid_class.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+from __future__ import division
+import rospy
+
+
+#####################################################
+#						PID							#
+#####################################################
+class PIDaxis():
+    def __init__(self, kp, ki, kd, kp_upper=None, i_range=None, d_range=None, control_range=(1000, 2000), midpoint=1500):
+        # Tuning
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        # Config
+        self.kp_upper = kp_upper
+        self.i_range = i_range
+        self.d_range = d_range
+        self.control_range = control_range
+        self.midpoint = midpoint
+        self.smoothing = True
+        # Internal
+        self._old_err = None
+        self._p = 0
+        self._i = 0
+        self._d = 0
+        self._dd = 0
+        self._ddd = 0
+
+    def step(self, err, time_elapsed, error=None):
+
+        if self._old_err is None:
+            # First time around prevent d term spike
+            self._old_err = err
+
+        # Find the p component
+        if self.kp_upper is not None and err < 0:
+            self._p = err * self.kp_upper
+        else:
+            self._p = err * self.kp
+
+        # Find the i component
+        self._i += err * self.ki * time_elapsed
+        if self.i_range is not None:
+            self._i = max(self.i_range[0], min(self._i, self.i_range[1]))
+
+        # Find the d component
+        self._d = (err - self._old_err) * self.kd / time_elapsed
+        if self.d_range is not None:
+            self._d = max(self.d_range[0], min(self._d, self.d_range[1]))
+        self._old_err = err
+
+        # Smooth over the last three d terms
+        if self.smoothing:
+            self._d = (self._d * 8.0 + self._dd * 5.0 + self._ddd * 2.0)/15.0
+            self._ddd = self._dd
+            self._dd = self._d
+
+        # Update p, i, d fields for the error message
+        if error is not None:
+            error.p = self._p
+            error.i = self._i
+            error.d = self._d
+
+        # Calculate control output
+        raw_output = self._p + self._i + self._d
+        output = min(max(raw_output + self.midpoint, self.control_range[0]), self.control_range[1])
+
+        return output
+
+
+class PID:
+
+    height_factor = 1.238
+    battery_factor = 0.75
+
+    def __init__(self,
+
+                 roll=PIDaxis(4., 4.0, 0.0, control_range=(1400, 1600), midpoint=1500),
+                 roll_low=PIDaxis(4., 0.2, 0.0, control_range=(1400, 1600), midpoint=1500),
+
+                 pitch=PIDaxis(4., 4.0, 0.0, control_range=(1400, 1600), midpoint=1500),
+                 pitch_low=PIDaxis(4., 0.2, 0.0, control_range=(1400, 1600), midpoint=1500),
+
+                 yaw=PIDaxis(0.0, 0.0, 0.0),
+
+                 # Kv 2300 motors have midpoint 1300, Kv 2550 motors have midpoint 1250
+                 throttle=PIDaxis(1.0/height_factor * battery_factor, 0.5/height_factor * battery_factor,
+                                  2.0/height_factor * battery_factor, kp_upper=1.0/height_factor * battery_factor,
+                                  i_range=(-400, 400), control_range=(1200, 2000), d_range=(-40, 40), midpoint=1250),
+                 throttle_low=PIDaxis(1.0/height_factor * battery_factor, 0.05/height_factor * battery_factor,
+                                      2.0/height_factor * battery_factor, kp_upper=1.0/height_factor * battery_factor,
+                                      i_range=(0, 400), control_range=(1200, 2000), d_range=(-40, 40), midpoint=1250)
+                 ):
+
+        self.trim_controller_cap_plane = 5.0
+        self.trim_controller_thresh_plane = 0.01
+
+        self.roll = roll
+        self.roll_low = roll_low
+
+        self.pitch = pitch
+        self.pitch_low = pitch_low
+
+        self.yaw = yaw
+
+        self.trim_controller_cap_throttle = 5.0
+        self.trim_controller_thresh_throttle = 5.0
+
+        self.throttle = throttle
+        self.throttle_low = throttle_low
+
+        self.sp = None
+        self._t = None
+
+        # Steve005 presets
+        self.roll_low._i = 15
+        self.pitch_low._i = 8
+
+        self.throttle_low.init_i = 130
+        self.throttle.init_i = 0.0
+        self.throttle.mw_angle_alt_scale = 1.0
+        self.reset()
+
+    def reset(self, state_controller=None):
+        self._t = None
+        self.throttle_low._i = self.throttle_low.init_i
+        self.throttle._i = self.throttle.init_i
+
+        if state_controller is not None:
+            state_controller.set_z = state_controller.initial_set_z
+
+    def step(self, error, cmd_yaw_velocity=0):
+        # First time around prevent time spike
+        if self._t is None:
+            time_elapsed = 1
+        else:
+            time_elapsed = rospy.get_time() - self._t
+
+        self._t = rospy.get_time()
+
+        # Compute roll command
+        if abs(error.x.err) < self.trim_controller_thresh_plane:
+            cmd_r = self.roll_low.step(error.x.err, time_elapsed, error.x)
+            self.roll._i = 0
+        else:
+            if error.x.err > self.trim_controller_cap_plane:
+                self.roll_low.step(self.trim_controller_cap_plane, time_elapsed, error.x)
+            elif error.x.err < -self.trim_controller_cap_plane:
+                self.roll_low.step(-self.trim_controller_cap_plane, time_elapsed, error.x)
+            else:
+                self.roll_low.step(error.x.err, time_elapsed, error.x)
+
+            cmd_r = self.roll_low._i + self.roll.step(error.x.err, time_elapsed, error.x)
+
+        # Compute pitch command
+        if abs(error.y.err) < self.trim_controller_thresh_plane:
+            cmd_p = self.pitch_low.step(error.y.err, time_elapsed, error.y)
+            self.pitch._i = 0
+        else:
+            if error.y.err > self.trim_controller_cap_plane:
+                self.pitch_low.step(self.trim_controller_cap_plane, time_elapsed, error.y)
+            elif error.y.err < -self.trim_controller_cap_plane:
+                self.pitch_low.step(-self.trim_controller_cap_plane, time_elapsed, error.y)
+            else:
+                self.pitch_low.step(error.y.err, time_elapsed, error.y)
+
+            cmd_p = self.pitch_low._i + self.pitch.step(error.y.err, time_elapsed, error.y)
+
+        # Compute yaw command
+        cmd_y = 1500 + cmd_yaw_velocity
+
+        # Compute throttle command
+        if abs(error.z.err) < self.trim_controller_thresh_throttle:
+            cmd_t = self.throttle_low.step(error.z.err, time_elapsed, error.z)
+            self.throttle_low._i += self.throttle._i
+            self.throttle._i = 0
+        else:
+            if error.z.err > self.trim_controller_cap_throttle:
+                self.throttle_low.step(self.trim_controller_cap_throttle, time_elapsed, error.z)
+            elif error.z.err < -self.trim_controller_cap_throttle:
+                self.throttle_low.step(-self.trim_controller_cap_throttle, time_elapsed, error.z)
+            else:
+                self.throttle_low.step(error.z.err, time_elapsed, error.z)
+
+            cmd_t = self.throttle_low._i + self.throttle.step(error.z.err, time_elapsed, error.z)
+
+            # jgo: this seems to mostly make a difference before the I term has
+            # built enough to be stable, but it really seems better with it. To
+            # see the real difference, compare cmd_t / mw_angle_alt_scale to
+            # cmd_t * mw_angle_alt_scale and see how it sinks. That happens to
+            # a less noticeable degree with no modification.
+            cmd_t = cmd_t / max(0.5, self.throttle.mw_angle_alt_scale)
+
+        # Print statements for the low and high i components
+        # print "Roll  low, hi:", self.roll_low._i, self.roll._i
+        # print "Pitch low, hi:", self.pitch_low._i, self.pitch._i
+        # print "Throttle low, hi:", self.throttle_low._i, self.throttle._i
+        return [cmd_r, cmd_p, cmd_y, cmd_t]
---
+++ scripts/old_pid_controller.py
@@ -0,0 +1,185 @@
+from pid_class import PID
+from pidrone_pkg.msg import RC, axes_err, Velocity, Mode, RC
+from geometry_msgs.msg import PoseStamped, TwistStamped
+from sensor_msgs.msg import Range, Imu
+import command_values
+import rospy
+import signal
+import sys
+import time
+import tf
+import numpy as np
+
+class PIDController(object):
+
+    def __init__(self, ctrlpub):
+        # Initial setpoint for the z-position of the drone
+        self.initial_set_z = 30.0
+        # Setpoint for the z-position of the drone
+        self.set_z = self.initial_set_z
+        # Current z-position of the drone according to sensor data
+        self.current_z = 0
+
+        # Tracks x, y velocity error and z-position error
+        self.error = axes_err()
+        # PID controller
+        self.pid = PID()
+
+        # Setpoint for the x-velocity of the drone
+        self.set_vel_x = 0
+        # Setpoint for the y-velocity of the drone
+        self.set_vel_y = 0
+
+        # Commanded yaw velocity of the drone
+        self.cmd_yaw_velocity = 0
+
+        # Tracks previous drone attitude
+        self.prev_angx = 0
+        self.prev_angy = 0
+        # Time of previous attitude measurement
+        self.prev_angt = None
+
+        # Angle compensation values (to account for tilt of drone)
+        self.mw_angle_comp_x = 0
+        self.mw_angle_comp_y = 0
+        self.mw_angle_alt_scale = 1.0
+        self.mw_angle_coeff = 10.0
+        self.angle = TwistStamped()
+
+        # Control Publisher
+        self.ctrlpub = ctrlpub
+
+        # Control values (initialize to disarmed values)
+        self.ctrl_vals = command_values.disarm_cmd
+
+        # Store the current mode of the drone
+        self.curr_mode = "DISARMED"
+
+    # ROS Subscriber Callback Methods
+    #################################
+    def mode_callback(self, msg):
+        """Update the prev and curr mode variables"""
+        self.curr_mode = msg.mode
+
+    def infrared_callback(self, msg):
+        """Updates the current z-position of the drone as measured by the infrared sensor"""
+        # Scales infrared sensor reading to get z accounting for tilt of the drone
+        self.current_z = msg.range * self.mw_angle_alt_scale * 100
+        self.error.z.err = self.set_z - self.current_z
+
+    def vrpn_callback(self, msg):
+        """Updates the current z-position of the drone as measured by the motion capture rig"""
+        # Mocap uses y-axis to represent the drone's z-axis motion
+        self.current_z = msg.pose.position.y * 100
+        self.error.z.err = self.set_z - self.current_z
+
+    def plane_callback(self, msg):
+        """Calculates error for x, y planar motion of the drone"""
+        self.error.x.err = (msg.twist.linear.x - self.mw_angle_comp_x) * self.current_z + self.set_vel_x
+        self.error.y.err = (msg.twist.linear.y + self.mw_angle_comp_y) * self.current_z + self.set_vel_y
+
+    def imu_callback(self, msg):
+        """Calculates angle compensation values to account for the tilt of the drone"""
+        # Convert the quaternion to euler angles
+        # store the orientation quaternion
+        oq = msg.orientation
+        orientation_list = [oq.x, oq.y, oq.z, oq.w]
+        (new_angx, new_angy, new_angz) = tf.transformations.euler_from_quaternion(orientation_list)
+        new_angt = time.time()
+
+        # Passes angle of drone and correct velocity of drone
+        self.angle.twist.angular.x = new_angx
+        self.angle.twist.angular.y = new_angy
+        self.angle.header.stamp = rospy.get_rostime()
+
+        dt = new_angt - self.prev_angt
+        self.mw_angle_comp_x = np.tan((new_angx - self.prev_angx) * dt) * self.mw_angle_coeff
+        self.mw_angle_comp_y = np.tan((new_angy - self.prev_angy) * dt) * self.mw_angle_coeff
+
+        self.mw_angle_alt_scale = np.cos(new_angx) * np.cos(new_angy)
+        self.pid.throttle.mw_angle_alt_scale = self.mw_angle_alt_scale
+
+        self.prev_angx = new_angx
+        self.prev_angy = new_angy
+        self.prev_angt = new_angt
+
+    def set_vel_callback(self,msg):
+        """Update the current fly velocities if the drone mode is FLYING"""
+        if self.curr_mode == 'FLYING':
+            self.update_fly_velocities(msg)
+
+    # Helper Methods
+    ################
+    def publish_ctrl(self, ctrl):
+        """Publish the controls to /pidrone/controller"""
+        msg = RC()
+        msg.roll = ctrl[0]
+        msg.pitch = ctrl[1]
+        msg.yaw = ctrl[2]
+        msg.throttle = ctrl[3]
+        self.ctrlpub.publish(msg)
+
+    def update_fly_velocities(self, msg):
+        """Updates the desired x, y, yaw velocities and z-position"""
+        if msg is not None:
+            self.set_vel_x = msg.x_velocity
+            self.set_vel_y = msg.y_velocity
+
+            self.cmd_yaw_velocity = msg.yaw_velocity
+
+            new_set_z = self.set_z + msg.z_velocity
+            if 0.0 < new_set_z < 49.0:
+                self.set_z = new_set_z
+
+    def ctrl_c_handler(self, signal, frame):
+        """Stops the controller"""
+        print "\nCaught ctrl-c! Stopping PIDController!"
+        sys.exit()
+
+def main():
+
+    # ROS Setup
+    ###########
+    rospy.init_node('pid_controller')
+
+    # Publishers
+    ############
+    anglepub = rospy.Publisher('/pidrone/angle', TwistStamped, queue_size=1)
+    ctrlpub = rospy.Publisher('/pidrone/controller', RC, queue_size=1)
+
+    # create PIDController object
+    pid_ctrlr = PIDController(ctrlpub)
+
+    # Subscribers
+    #############
+    rospy.Subscriber("/pidrone/plane_err", TwistStamped, pid_ctrlr.plane_callback)
+    rospy.Subscriber("/pidrone/infrared", Range, pid_ctrlr.infrared_callback)
+    rospy.Subscriber("/pidrone/vrpn_pos", PoseStamped, pid_ctrlr.vrpn_callback)
+    rospy.Subscriber("/pidrone/set_vel", Velocity, pid_ctrlr.set_vel_callback)
+    rospy.Subscriber("/pidrone/imu", Imu, pid_ctrlr.imu_callback)
+    rospy.Subscriber("/pidrone/mode", Mode, pid_ctrlr.mode_callback)
+
+    # Non-ROS Setup
+    ###############
+    signal.signal(signal.SIGINT, pid_ctrlr.ctrl_c_handler)
+
+    pid_ctrlr.prev_angt = time.time()
+
+    print 'PID controller started'
+    while not rospy.is_shutdown():
+        if not pid_ctrlr.curr_mode == 'DISARMED':
+            anglepub.publish(pid_ctrlr.angle)
+
+        # Use a PID controller to calculate the flight command: [roll, pitch, yaw, throttle]
+        fly_ctrl = pid_ctrlr.pid.step(pid_ctrlr.error, pid_ctrlr.cmd_yaw_velocity)
+
+        if pid_ctrlr.curr_mode == 'FLYING':
+            pid_ctrlr.publish_ctrl(fly_ctrl)
+        elif pid_ctrlr.curr_mode == 'ARMED':
+                pid_ctrlr.pid.reset(pid_ctrlr)
+
+    print 'Stopping PID controller'
+
+
+if __name__ == '__main__':
+    main()
