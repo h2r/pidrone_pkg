64319115b8ad586f94e0e1eab24d0ba18807b817
Garrett Warren <garrettwarren@garretts-mbp.rlab.cs.brown.edu>
2018-08-10 10:34:40 -0400

    made the entry case insensitive

--- scripts/command_line_interface.py
+++ scripts/command_line_interface.py
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     desired_twist_pub = rospy.Publisher('/pidrone/desired/twist', Twist, queue_size=1)
     reset_transform_pub = rospy.Publisher('pidrone/reset_transform', Empty, queue_size=1)
     toggle_transform_pub = rospy.Publisher('pidrone/toggle_transform', Bool, queue_size=1)
+    position_control = False
 
-    toggle = False
     publish_desired_mode('DISARMED', desired_mode_pub)
     signal.signal(signal.SIGINT, lambda x,y: ctrl_c_handler(x,y,desired_mode_pub))
     print('Valid modes are DISARMED, ARMED, and FLYING')
@@ -71,33 +71,40 @@ if __name__ == '__main__':
         while not rospy.is_shutdown():
             raw_entry = raw_input('Type a mode and press enter:\t')
             entry = raw_entry.strip()
+            entry = entry.lower()
             if len(entry) == 0:
                 print 'invalid entry. try again.'
             else:
                 # position commands take the form p <x> <y> <z> where x, y, and z are floats
                 if entry[0] == 'r' and len(entry) == 1:
                     reset_transform_pub.publish(Empty())
-                elif entry[0] == 'p' and len(entry) == 1:
-                    toggle = not toggle
-                    toggle_transform_pub.publish(toggle)
-                elif entry[0] == 'p':
-                    # It's a position command
-                    strs = entry.split()
-                    if len(strs) >= 4:
-                        x = float(strs[1])
-                        y = float(strs[2])
-                        z = float(strs[3])
-                        publish_desired_pose(x, y, z, desired_pose_pub)
 
-                # position commands take the form v <x> <y> <z> where x, y, and z are floats
+                # it's a position command
+                elif entry[0] == 'p':
+                    # turn on position controll
+                    postion_control = True
+                    toggle_transform_pub.publish(position_control)
+                    if len(entry) > 1:
+                    # set the desired position
+                        strs = entry.split()
+                        if len(strs) >= 4:
+                            x = float(strs[1])
+                            y = float(strs[2])
+                            z = float(strs[3])
+                            publish_desired_pose(x, y, z, desired_pose_pub)
+
+                # veloity commands take the form v <x> <y> <z> where x, y, and z are floats
                 elif entry[0] == 'v':
-                    # It's a velocity command
-                    strs = entry.split()
-                    if len(strs) >= 4:
-                        vx = float(strs[1])
-                        vy = float(strs[2])
-                        vz = float(strs[3])
-                        publish_desired_twist(vx, vy, vz, desired_twist_pub)
+                    position_control = False
+                    toggle_transform_pub.publish(position_control)
+                    if len(entry) > 1:
+                    # set the desired velocity
+                        strs = entry.split()
+                        if len(strs) >= 4:
+                            vx = float(strs[1])
+                            vy = float(strs[2])
+                            vz = float(strs[3])
+                            publish_desired_twist(vx, vy, vz, desired_twist_pub)
 
                 # mode commands take the form of a string or letter. possible
                 # entries are:
@@ -106,26 +113,23 @@ if __name__ == '__main__':
                 # fly : 'FLYING', 'F'
                 else:
                     # It's a mode command
-                    if 'x' in entry:
-                        desired_mode = 'DISARMED'
-                        publish_desired_mode(desired_mode, desired_mode_pub)
-                    elif (entry == 'DISARMED') or (entry == 'D'):
-                        desired_mode = 'DISARMED'
-                        publish_desired_mode(desired_mode, desired_mode_pub)
-                    elif (entry == 'ARMED') or (entry == 'A'):
-                        desired_mode = 'ARMED'
-                        publish_desired_mode(desired_mode, desired_mode_pub)
-                    elif (entry == 'FLYING') or (entry == 'F'):
-                        desired_mode = 'FLYING'
-                        publish_desired_mode(desired_mode, desired_mode_pub)
-                    elif (entry == 'x') or (entry == 'X'): # This is the 'panic' command
+                    if 'x' in entry: # This is the 'panic' command
                         desired_mode = 'DISARMED'
                         publish_desired_mode(desired_mode, desired_mode_pub)
                         break
+                    elif (entry == 'DISARMED') or (entry == 'd'):
+                        desired_mode = 'DISARMED'
+                        publish_desired_mode(desired_mode, desired_mode_pub)
+                    elif (entry == 'ARMED') or (entry == 'a'):
+                        desired_mode = 'ARMED'
+                        publish_desired_mode(desired_mode, desired_mode_pub)
+                    elif (entry == 'FLYING') or (entry == 'f'):
+                        desired_mode = 'FLYING'
+                        publish_desired_mode(desired_mode, desired_mode_pub)
                     else:
                         print 'invalid command. try again'
 
     except Exception as ex:
         # Disarm drone on exception
         publish_desired_mode('DISARMED', desired_mode_pub)
-        raise(ex)
+        raise
