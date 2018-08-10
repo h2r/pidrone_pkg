22afa6e8ac3996ffb71e1a52c34bca1488e96bbb
Garrett Warren <garrettwarren@garretts-mbp.rlab.cs.brown.edu>
2018-08-10 12:26:22 -0400

    Corrected range reading based on drone attitude

--- scripts/state_estimator_ema.py
+++ scripts/state_estimator_ema.py
@@ -3,6 +3,7 @@ import tf
 import sys
 import rospy
 import signal
+import numpy as np
 from pidrone_pkg.msg import State
 from sensor_msgs.msg import Range, Imu
 from geometry_msgs.msg import Pose, Twist
@@ -44,6 +45,8 @@ class EMAStateEstimator(object):
         self.analyze_pose_is_transforming_on_first_image = False
 
 
+    # ROS Subscriber Callback Methods:
+    ##################################
     def twist_callback(self, data):
         """ Update the twist of the drone
         Does not alter the angular velocities because these are set by imu
@@ -56,19 +59,8 @@ class EMAStateEstimator(object):
         # update that data has been recieved
         self.received_twist_data = True
 
-    def filter_twist(self, twist):
-        """ Run an ema filter on the velocity data and update the state velocity """
-        # measured planar velocities of the drone
-        new_vel = twist.linear
-        # the current planar velocities of the drone
-        velocity = self.state.twist_with_covariance.twist.linear
-        # the constant for the ema filter
-        alpha = 0.8
-        velocity.x = (1.0 - alpha) * velocity.x + alpha * new_vel.x * position.z
-        velocity.y = (1.0 - alpha) * velocity.y + alpha * new_vel.y * position.z
-        self.state.twist_with_covariance.twist.linear = velocity
-
     def reset_callback(self, empty):
+        """ Reset the current pose of the drone except for the z postion """
         self.state.header.stamp = rospy.Time.now()
         print 'Resetting position in x and y and orientation'
         self.state.pose_with_covariance.pose.position.x = 0
@@ -77,14 +69,31 @@ class EMAStateEstimator(object):
         self.state.pose_with_covariance.pose.orientation = Pose().orientation
 
     def pose_callback(self, data):
-        ''' Update the pose of the drone based on the translations published
+        """ Update the pose of the drone based on the translations published
         by analyze_pose
-        '''
+        """
         # update the header stamp
         self.state.header.stamp = rospy.Time.now()
         # update the pose data
         self.filter_pose(data)
 
+    def range_callback(self, data):
+        """ Update the z-position of the drone """
+        # update the z position
+        self.filter_range(data.range)
+        # set received range data to True
+        self.received_range_data = True
+
+    def tofi_callback(self, msg):
+        """ Store whether or not the pose data is from the first image. This
+        determines whether the pose data is a position esitimate based on the
+        first image, or an estimate of the translation from the previous
+        image
+        """
+        self.transforming_on_first_image = msg.data
+
+    # EMA Filtering Methods:
+    ########################
     def filter_pose(self, pose):
         """ Calculate the position of the drone based on the pose translations
         using an EMA filter if the pose is based off of the first image, or by
@@ -109,35 +118,52 @@ class EMAStateEstimator(object):
 
         self.state.pose_with_covariance.pose.position = position
 
-
-    def range_callback(self, data):
-        ''' Update the z position of the drone '''
-        # update the z position
-        self.filter_range(data.range)
-        # set received range data to True
-        self.received_range_data = True
-
-# TODO modify value based on roll and pitch values
-    def filter_range(self, range_reading):
-        ''' Smoothe the range reading using and ema filter '''
-        # the ema filter constant
-        alpha = 0.2
-        # store the current z position of the drone
-        current_range = self.state.pose_with_covariance.pose.position.z
-        # use an ema filter to smoothe the range reading
-        smoothed_range = (1.0 - alpha) * range_reading + alpha * current_range
-        # ensure that the range value is between 0 and 0.55 m
-        smoothed_range = max(0, min(smoothed_range, 0.55))
-        # update the current z position
-        self.state.pose_with_covariance.pose.position.z = smoothed_range
-
     def imu_callback(self, data):
         """ Update the attitude of the drone """
         self.state.pose_with_covariance.pose.orientation = data.orientation
         self.state.twist_with_covariance.twist.angular = data.angular_velocity
 
-    def tofi_callback(self, msg):
-        self.transforming_on_first_image = msg.data
+    def filter_twist(self, twist):
+        """ Run an ema filter on the velocity data and update the state velocity """
+        # measured planar velocities of the drone
+        new_vel = twist.linear
+        # the current planar velocities of the drone
+        velocity = self.state.twist_with_covariance.twist.linear
+        # the constant for the ema filter
+        alpha = 0.8
+        velocity.x = (1.0 - alpha) * velocity.x + alpha * new_vel.x * position.z
+        velocity.y = (1.0 - alpha) * velocity.y + alpha * new_vel.y * position.z
+        self.state.twist_with_covariance.twist.linear = velocity
+
+    def filter_range(self, range_reading):
+        """ Smoothe the range reading using and ema filter """
+        # the ema filter constant
+        alpha = 0.2
+        # get the roll and pitch
+        r,p,_ = self.get_r_p_y()
+        # the z-position of the drone which is calculated by multiplying the
+        # the range reading by the cosines of the roll and pitch
+        curr_altitude = range_reading * np.cos(r) * np.cos(p)
+        prev_altitude = self.state.pose_with_covariance.pose.position.z
+        # use an ema filter to smoothe the range reading
+        smoothed_altitude= (1.0 - alpha) * curr_altitude + alpha * prev_altitude
+        # ensure that the range value is between 0 and 0.55 m
+        smoothed_altitude = max(0, min(smoothed_altitude, 0.55))
+        # update the current z position
+        self.state.pose_with_covariance.pose.position.z = smoothed_altitude
+
+
+    # Helper Methods:
+    #################
+    def get_r_p_y(self):
+        """ Return the roll, pitch, and yaw from the orientation quaternion """
+        x = self.state.pose_with_covariance.pose.orientation.x
+        y = self.state.pose_with_covariance.pose.orientation.y
+        z = self.state.pose_with_covariance.pose.orientation.z
+        w = self.state.pose_with_covariance.pose.orientation.w
+        quaternion = (x,y,z,w)
+        r,p,y = tf.transformations.euler_from_quaternion(quaternion)
+        return r,p,y
 
     def ctrl_c_handler(self, signal, frame):
         """ Exit the program """
