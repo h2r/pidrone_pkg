f3eabc11bec5debb63251c17b07604ca806e83ef
Sophie Yang <sophie_yang@brown.edu>
2018-08-15 13:27:26 -0400

    Made faster with numpy

--- scripts/object_tracking.py
+++ scripts/object_tracking.py
@@ -2,29 +2,40 @@ from __future__ import division
 import cv2
 from pidrone_pkg.msg import State
 from sensor_msgs.msg import Image
-from std_msgs.msg import Empty, Pose
-import rospy
-import math
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Pose
+from analyze_flow import AnalyzeFlow
 from cv_bridge import CvBridge
+import rospy
+import picamera
+import picamera.array
+import numpy as np
+import sys
+
+CAMERA_WIDTH = 320
+CAMERA_HEIGHT = 240
+CAMERA_CENTER = np.float32([(CAMERA_WIDTH - 1) / 2., (CAMERA_HEIGHT - 1) / 2.])
 
 
-class ObjectTracker:
-    def __init__(self):
-        self.bridge = CvBridge()
+class ObjectTracker(picamera.array.PiMotionAnalysis):
+    def __init__(self, camera):
+        picamera.array.PiMotionAnalysis.__init__(self, camera)
 
-        self.obj_pos_pub = rospy.Publisher('/pidrone/desired/pose', Pose, queue_size=1)
+        self.obj_pose_pub = rospy.Publisher('/pidrone/desired/pose', Pose, queue_size=1)
 
-        self.detector = cv2.ORB(nfeatures=200, scoreType=cv2.ORB_FAST_SCORE)
+        self.detector = cv2.ORB(nfeatures=150, scoreType=cv2.ORB_FAST_SCORE)
 
         self.curr_obj_coordinates = None
-
+        self.error = None
         self.track_object = False
+
+        self.prev_img = None
 
         self.alpha = 0.70
         self.z = 0.16
 
-    def image_callback(self, data):
-        curr_img = self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+    def write(self, data):
+        curr_img = np.reshape(np.fromstring(data, dtype=np.uint8), (CAMERA_HEIGHT, CAMERA_WIDTH, 3))
 
         # start object tracking
         if self.track_object:
@@ -38,24 +49,25 @@ class ObjectTracker:
                     kp_x.append(curr_kp[i].pt[0])
                     kp_y.append(curr_kp[i].pt[1])
 
-                obj_coordinates, density = find_densest_point(kp_x, kp_y)
+                obj_coordinates, density = find_densest_point(np.array(kp_x), np.array(kp_y))
 
                 if self.curr_obj_coordinates is not None:
-                    self.curr_obj_coordinates[0] = self.alpha*self.curr_obj_coordinates[0] + (1-self.alpha)*obj_coordinates[0]
-                    self.curr_obj_coordinates[1] = self.alpha*self.curr_obj_coordinates[1] + (1-self.alpha)*obj_coordinates[1]
+                    self.curr_obj_coordinates = self.alpha*self.curr_obj_coordinates + (1-self.alpha)*obj_coordinates
                 else:
                     self.curr_obj_coordinates = obj_coordinates
 
-                self.curr_obj_coordinates = self.curr_obj_coordinates * self.z / 290.
-                print "object coordinates: ", self.curr_obj_coordinates
+                self.error = (np.subtract(self.curr_obj_coordinates, CAMERA_CENTER) * self.z) / 290. * np.array((1, -1))
+                print "ERROR: ", self.error
                 print "density: ", density
 
                 obj_pose = Pose()
-                obj_pose.position.x = self.curr_obj_coordinates[0]
-                obj_pose.position.y = self.curr_obj_coordinates[1]
+                obj_pose.position.x = self.error[0]
+                obj_pose.position.y = self.error[1]
                 self.obj_pose_pub.publish(obj_pose)
             else:
                 print "CANNOT FIND ANY FEATURES !!!!!"
+
+        self.prev_img = curr_img
 
     def reset_callback(self, data):
         if not self.track_object:
@@ -76,57 +88,66 @@ def find_densest_point(x, y, r=50, max_density_point=None, max_density=None, ite
 
         points_x = x
         points_y = y
-        iteration = iteration + 1
-        max_density_index = None
-
-        max_density = -float("inf")
+        iteration += 1
 
         num_points = len(points_x)
 
-        distances = [[0 for i in range(num_points)] for i in range(num_points)]
+        z = np.array([complex(points_x[i], points_y[i]) for i in range(num_points)])
+        distances = abs(z[..., np.newaxis]-z)
 
-        for i in range(num_points):
-            for j in range(i, num_points):
-                d = calc_distance(points_x[i], points_y[i], points_x[j], points_y[j])
-                distances[i][j] = d
-                distances[j][i] = d
-
-        for i in range(num_points):
-            density = 0
-            for j in range(num_points):
-                density = density + math.exp((-distances[i][j] ** 2) / (r ** 2))
-            if density > max_density:
-                max_density = density
-                max_density_index = i
-
-        max_density_point = [points_x[max_density_index], points_y[max_density_index]]
+        densities = np.exp((-distances ** 2) / (r**2)).sum(axis=1)
+        max_density_index = densities.argmax()
+        max_density_point = np.array([points_x[max_density_index], points_y[max_density_index]])
+        max_density = densities.max()
 
         local_r = 50.0
-        l_points_x = []
-        l_points_y = []
 
-        for i in range(num_points):
-            if distances[max_density_index][i] <= local_r:
-                l_points_x.append(points_x[i])
-                l_points_y.append(points_y[i])
+        mask = distances[max_density_index] <= local_r
+        l_points_x = points_x[mask]
+        l_points_y = points_y[mask]
 
         return find_densest_point(l_points_x, l_points_y, 50, max_density_point, max_density, iteration)
 
     return max_density_point, max_density
 
 
-def calc_distance(x1, y1, x2, y2):
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
-
 def main():
     rospy.init_node("object_tracking")
-    tracker = ObjectTracker()
-    rospy.Subscriber("/pidrone/reset_transform", Empty, tracker.reset_callback)
-    rospy.Subscriber("/pidrone/picamera/image_raw", Image, tracker.image_callback)
-    rospy.Subscriber("/pidrone/state", State, tracker.state_callback)
 
-    rospy.spin()
+    image_pub = rospy.Publisher("/pidrone/picamera/image_raw", Image, queue_size=1, tcp_nodelay=False)
+
+    try:
+
+        bridge = CvBridge()
+
+        with picamera.PiCamera(framerate=90) as camera:
+            camera.resolution = (320, 240)
+            with ObjectTracker(camera) as tracker:
+
+                rospy.Subscriber("/pidrone/reset_transform", Empty, tracker.reset_callback)
+                rospy.Subscriber("/pidrone/state", State, tracker.state_callback)
+
+                with AnalyzeFlow(camera) as flow_analyzer:
+                    # run the setup functions for each of the image callback classes
+                    flow_analyzer.setup(camera.resolution)
+
+                    # start the recordings for the image and the motion vectors
+                    camera.start_recording("/dev/null", format='h264', splitter_port=1, motion_output=flow_analyzer)
+                    camera.start_recording(tracker, format='bgr', splitter_port=2)
+
+                    while not rospy.is_shutdown():
+                        camera.wait_recording(1 / 100.0)
+
+                        if tracker.prev_img is not None:
+                            image_message = bridge.cv2_to_imgmsg(tracker.prev_img, encoding="bgr8")
+                            image_pub.publish(image_message)
+
+                camera.stop_recording(splitter_port=1)
+            camera.stop_recording(splitter_port=2)
+        print "Shutdown Received"
+        sys.exit()
+    except Exception as e:
+        raise
 
 
 if __name__ == "__main__":
