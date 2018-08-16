9e314c0d4b9d831fcd3ec6a91b2b1ef33abc0ded
Josh Roy <josh_roy@brown.edu>
2018-08-16 00:05:38 -0700

    Fixed prints, added some comments

--- scripts/state_estimator_ema.py
+++ scripts/state_estimator_ema.py
@@ -58,18 +58,22 @@ class EMAStateEstimator(object):
         """
         # update the header stamp
         self.state.header.stamp = data.header.stamp
+
         # update linear twist data
-#TODO TEST
+#TODO TEST jroy1: lol
         self.filter_twist(data.twist)
+
         # update that data has been recieved
         self.received_twist_data = True
 
     def reset_callback(self, empty):
         """ Reset the current pose of the drone except for the z postion """
         self.state.header.stamp = rospy.Time.now()
-        print 'Resetting position in x and y and orientation'
+
+        print('Resetting position in x and y and orientation')
         self.state.pose_with_covariance.pose.position.x = 0
         self.state.pose_with_covariance.pose.position.y = 0
+
         # reset the orientation
         self.state.pose_with_covariance.pose.orientation = Pose().orientation
 
@@ -79,6 +83,7 @@ class EMAStateEstimator(object):
         """
         # update the header stamp
         self.state.header.stamp = data.header.stamp
+
         # update the pose data
         self.filter_pose(data.pose)
 
@@ -86,6 +91,7 @@ class EMAStateEstimator(object):
         """ Update the z-position of the drone """
         # update the z position
         self.filter_range(data.range)
+
         # set received range data to True
         self.received_range_data = True
 
@@ -106,12 +112,17 @@ class EMAStateEstimator(object):
         """
         # current position of the drone
         position = self.state.pose_with_covariance.pose.position
+
         # raw measured translations and rotation by analyze_transform
         translation = pose.position
+
         # constant used for the EMA filter
+        # jroy1: Should be a class variable
         alpha = 0.2
+
         # if the measurement is based off of the first image
         if self.analyze_pose_is_transforming_on_first_image:
+
             # blend the new measurement with the old position using an EMA filter
             position.x = (1.0 - alpha) * position.x + alpha * translation.x * position.z
             position.y = (1.0 - alpha) * position.y + alpha * translation.y * position.z
@@ -132,14 +143,20 @@ class EMAStateEstimator(object):
         """ Run an ema filter on the velocity data and update the state velocity """
         # current position of the drone
         altitude = self.state.pose_with_covariance.pose.position.z
+
         # measured planar velocities of the drone
         new_vel = twist.linear
+
         # the current planar velocities of the drone
         velocity = self.state.twist_with_covariance.twist.linear
+
         # update the angular velocity coefficients to account for the angular
         # motion of the drone
         self.calc_angle_comp_values()
+
         # the constant for the ema filter
+        # jroy1: Should be a (differently named) class variable
+        alpha = 0.2
         alpha = 0.4
         velocity.x = self.near_zero((1.0 - alpha) * velocity.x + alpha * (new_vel.x * altitude - self.mw_angle_comp_x))
         velocity.y = self.near_zero((1.0 - alpha) * velocity.y + alpha * (new_vel.y * altitude - self.mw_angle_comp_y))
@@ -147,18 +164,25 @@ class EMAStateEstimator(object):
 
     def filter_range(self, range_reading):
         """ Smoothe the range reading using and ema filter """
+
         # the ema filter constant
+        # jroy1: Should be a (differently named) class variable
         alpha = 0.8
+
         # get the roll and pitch
         r,p,_ = self.get_r_p_y()
+
         # the z-position of the drone which is calculated by multiplying the
         # the range reading by the cosines of the roll and pitch
         curr_altitude = range_reading * np.cos(r) * np.cos(p)
         prev_altitude = self.state.pose_with_covariance.pose.position.z
+
         # use an ema filter to smoothe the range reading
         smoothed_altitude= (1.0 - alpha) * curr_altitude + alpha * prev_altitude
+
         # ensure that the range value is between 0 and 0.55 m
         smoothed_altitude = max(0, min(smoothed_altitude, 0.55))
+
         # update the current z position
         self.state.pose_with_covariance.pose.position.z = smoothed_altitude
 
@@ -184,13 +208,16 @@ class EMAStateEstimator(object):
         self.mw_angle_comp_x = angular_velocities.x * altitude * self.mw_angle_coeff
         self.mw_angle_comp_y = angular_velocities.y * altitude * self.mw_angle_coeff
 
+    # jroy1: I'm noticing this function is being used in multiple files with
+    # different thresholds. Let's abstract it to a file containing generic
+    # helpers
     def near_zero(self, n):
         """ Set a number to zero if it is below a threshold value """
         return 0.0 if abs(n) < 1e-6 else n
 
     def ctrl_c_handler(self, signal, frame):
         """ Exit the program """
-        print '\nCaught ctrl-c. Stopping node.'
+        print('\nCaught ctrl-c. Stopping node.')
         sys.exit()
 
 if __name__ == '__main__':
@@ -218,11 +245,11 @@ if __name__ == '__main__':
 
     # set up ctrl-c handler
     signal.signal(signal.SIGINT, state_estimator.ctrl_c_handler)
-    print 'waiting for velocity and range data'
+    print('waiting for velocity and range data')
     while not state_estimator.received_twist_data and \
           not state_estimator.received_range_data:
         pass
-    print 'Publishing State'
+    print('Publishing State')
 
     # set the publishing rate (Hz)
     rate = rospy.Rate(100)
