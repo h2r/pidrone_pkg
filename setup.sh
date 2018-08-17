3c8a0120bf3d43731bdf713b5cccbff3421f0aff
Garrett Warren <garrettmichaelwarren@gmail.com>
2018-08-17 13:02:25 -0400

    made hostname generic

--- setup.sh
+++ setup.sh
@@ -7,8 +7,7 @@
 # also set any other ROS environment variables here.
 
 source ../../devel/setup.bash
-#export ROS_IP=192.168.42.1
-export ROS_HOSTNAME=tdrone-blue
-export ROS_MASTER_URI=http://tdrone-blue:11311
+export ROS_IP=192.168.42.1
+#export ROS_HOSTNAME=enterhostnamehere
+#export ROS_MASTER_URI=http://enterhostnamehere:11311
 export PS1="\[\033[00;33m\][pidrone - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
-
