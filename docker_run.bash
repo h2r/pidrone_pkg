34d0a3f0cd78f4971bbfa2770277246cc7b1af36
root <baichuan55555@gmail.com>
2018-09-09 17:38:43 +0000

    add docker run

--- docker_run.bash
+++ docker_run.bash
@@ -1,9 +1,10 @@
 sudo docker run -it \
     --net=host \
-    --name="drone" \
+    --name="pidrone" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    osrf/ros:indigo-desktop-full \
+    --device /dev/snd:/dev/snd \
+    pidrone \
     bash
 export containerId=$(sudo docker ps -l -q)
