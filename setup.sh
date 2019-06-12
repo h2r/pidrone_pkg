3da9bb024635a2f42b57b95f419d039862ed6400
nkumar15-brown-university <43328693+nkumar15-brown-university@users.noreply.github.com>
2019-06-12 14:07:05 -0400

    [#7] Undid my hardcoded values in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@
 
 source ../../devel/setup.bash
 export ROS_IP=192.168.42.1
-export ROS_HOSTNAME=nkdrone
-export ROS_MASTER_URI=http://${ROS_HOSTNAME}:11311
+#export ROS_HOSTNAME=<enterhostnamehere>
+#export ROS_MASTER_URI=http://<enterhostnamehere>:11311
 export PS1="\[\033[00;33m\][pidrone - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
