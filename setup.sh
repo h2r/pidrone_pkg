56de4456ce0c4fb84bc85813f7709d2d18c3b501
nkumar15-brown-university <43328693+nkumar15-brown-university@users.noreply.github.com>
2019-06-15 16:31:08 -0400

    [#11] Changed ROS_HOSTNAME and ROS_MASTER_URI to use machine's hostname

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@
 # also set any other ROS environment variables here.
 
 source ../../devel/setup.bash
+[ -n "`hostname`" ] && my_hostname="`hostname`" || my_hostname='defaultdrone'
 export ROS_IP=192.168.42.1
-#export ROS_HOSTNAME=<enterhostnamehere>
-#export ROS_MASTER_URI=http://<enterhostnamehere>:11311
+export ROS_HOSTNAME="${my_hostname}"
+export ROS_MASTER_URI="http://${my_hostname}:11311"
 export PS1="\[\033[00;33m\][pidrone - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
