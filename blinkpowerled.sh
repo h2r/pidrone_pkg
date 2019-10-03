dba68f491cad2f30e1f5ebb659a26f1e98452ebe
David Boles <me@davidbol.es>
2019-10-03 12:09:32 -0400

    Correct power LED number in blink script.

--- blinkpowerled.sh
+++ blinkpowerled.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-echo gpio | sudo tee /sys/class/leds/led1/trigger
+echo gpio | sudo tee /sys/class/leds/led0/trigger
 
-echo 1 | sudo tee /sys/class/leds/led1/brightness
+echo 1 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
-echo 0 | sudo tee /sys/class/leds/led1/brightness
+echo 0 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
-echo 1 | sudo tee /sys/class/leds/led1/brightness
+echo 1 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
-echo 0 | sudo tee /sys/class/leds/led1/brightness
+echo 0 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
-echo 1 | sudo tee /sys/class/leds/led1/brightness
+echo 1 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
-echo 0 | sudo tee /sys/class/leds/led1/brightness
+echo 0 | sudo tee /sys/class/leds/led0/brightness
 sleep 1
 
 
-echo input | sudo tee /sys/class/leds/led1/trigger
+echo input | sudo tee /sys/class/leds/led0/trigger
