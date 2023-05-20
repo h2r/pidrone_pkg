73f76438c7a2d5203d8ab429c193025f81d27c15
Davide Iafrate <Tuxliri@users.noreply.github.com>
2023-05-20 16:18:00 +0200

    Uniformed optical_flow_node.py to student code structure

--- scripts/optical_flow_node.py
+++ scripts/optical_flow_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 
 import rospy
@@ -20,21 +20,22 @@ class OpticalFlowNode(object):
     def __init__(self, node_name):
 
         rospy.init_node(node_name)
-        self.twistpub = rospy.Publisher('/pidrone/picamera/twist', TwistStamped, queue_size=1)
         # flow variables
         camera_wh = (320, 240)        
         self.max_flow = camera_wh[0] / 16.0 * camera_wh[1] / 16.0 * 2**7
         self.flow_scale = .165
         self.flow_coeff = 100 * self.flow_scale / self.max_flow # (multiply by 100 for cm to m conversion)
-
         self.altitude = 0.03 # initialize to a bit off the ground
         self.altitude_ts = rospy.Time.now()
+        self.setup()
+
+    def setup(self):
+        # publisher
+        self.twistpub = rospy.Publisher('/pidrone/picamera/twist', TwistStamped, queue_size=1)
 
         # subscribers
         self._sub_mv = rospy.Subscriber('/raspicam_node/motion_vectors', MotionVectors, self.motion_cb, queue_size=1)
         self._sub_alt = rospy.Subscriber('/pidrone/range', Range, self.altitude_cb, queue_size=1)
-
-
 
     def motion_cb(self, msg):
         ''' Average the motion vectors and publish the
